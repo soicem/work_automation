5e682db741801df2552d6336c3393be6c3136a4c
KIM NAMGUE <knq512412@gmail.com>
2020-02-27 10:44:00 +0900

    Update excelPriceDuplicationChecker.py

--- excelPriceDuplicationChecker.py
+++ excelPriceDuplicationChecker.py
@@ -4,11 +4,6 @@ import pandas as pd
 
 nameOfSrc = "20200226_26140_개별주택_조사목록(부산 서구) 다수동 시청제출(1)"
 df_src = pd.read_excel(nameOfSrc + ".xls")
-#print(df_src[["지번", "동구분", "20년 토지단가"]].iloc[0])
-#print(len(df_src))
-#df = pd.DataFrame({'Data': result}) 
-#df.to_excel(nameOfSrc + "_접면" + ".xls")
-print(df_src[["읍면동", "지번", "동구분", "20년 토지단가"]].iloc[0]["지번"])
 duplication = {}
 
 for i in range(0, len(df_src)):
@@ -35,15 +30,12 @@ for i in range(0, len(df_src)):
     seperator = current["동구분"]
     price = current["20년 토지단가"]
     keyOfCurrent = place + " " + number
-    st = xlwt.easyxf('pattern: pattern solid;')
-    if duplication[keyOfCurrent] == -1:
-        st.pattern.pattern_fore_colour = 80
-    else:
-        st.pattern.pattern_fore_colour = 100
-    #sheet1.write(i, 0, place, st)
+    st = xlwt.easyxf('pattern: pattern solid, fore_colour yellow;')
+    if duplication[keyOfCurrent] != -1:
+        st = xlwt.easyxf('pattern: pattern solid, fore_colour white;')
     sheet1.write(i, 0, number, st)
+    sheet1.write(i, 1, price, st)
     sheet1.write(i, 2, price, st)
-    sheet1.write(i, 4, price, st)
     
 book.save('simple.xls')
 
